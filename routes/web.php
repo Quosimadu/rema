446459d98021860e4cce66beda63a311f58e0db2
Quosimadu <paul.wehle@pcwehle.de>
2017-05-04 22:13:55 +0200

    bugfix basics refs #2

--- routes/web.php
+++ routes/web.php
@@ -74,6 +74,6 @@ Route::group(array('prefix' => 'reports', 'middleware' => 'auth'), function() {
 
 Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
 
-Route::post('receive-sms', ['uses' => 'MessageController@receiveSMS']);
+Route::post('receive-sms', ['uses' => 'MessagesController@receiveSMS']);
 
 
