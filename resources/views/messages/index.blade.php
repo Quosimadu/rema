bf5d56048e53033fdd48fa93b60e37ed636b7875
Paul Wehle <paul.wehle@pcwehle.de>
2017-05-11 18:08:31 +0200

    update message show view, move compose button to the top refs #2

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -9,10 +9,12 @@
                         <h1>Messages</h1>
                     </div>
                     <div class="panel-body">
+                        <p><a class="btn btn-primary" href="{!! route('messages.compose') !!}">Compose message</a></p>
                         <table class="table">
                             <thead>
                             <tr>
                                 <th>Date</th>
+                                <th>Sender</th>
                                 <th>Receiver</th>
                                 <th>Content</th>
                             </tr>
@@ -20,13 +22,13 @@
                             @foreach($messages as $message)
                                 <tr>
                                     <td>{!! $message->created_at !!}</td>
+                                    <td>{!! $message->sender !!}</td>
                                     <td>{!! $message->receiver !!}</td>
                                     <td><a href="{!! route('messages.show', $message->id) !!}">{!! $message->content !!}</a></td>
                                 </tr>
 
                             @endforeach
                         </table>
-                        <p><a class="btn btn-primary" href="{!! route('messages.compose') !!}">Add new message</a></p>
                     </div>
                 </div>
             </div>
--- resources/views/messages/show.blade.php
+++ resources/views/messages/show.blade.php
@@ -6,11 +6,13 @@
             <div class="col-md-12">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        <h1>{!! $message->sender !!} &gt; {!! $message->receiver !!} <i>({!! $message->created_at !!})</i></h1>
+                        <h1>Message details</h1>
                     </div>
                     <div class="panel-body">
                         <p><a class="btn btn-link" href="{!! route('messages') !!}">Back to messages overview</a></p>
-
+                        <p>Sender: {!! $message->sender !!} &gt;</p>
+                        <p>Receiver: {!! $message->receiver !!}</p>
+                        <p><i>Sent: {!! $message->created_at !!}</i></p>
                         <p>{!! $message->content !!}</p>
 
                     </div>
