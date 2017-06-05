7b170354c32e9fe529566bc74a383476875000d9
Paul Wehle <paul.wehle@pcwehle.de>
2017-06-05 14:21:39 +0200

    debug logout refs #4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -70,7 +70,9 @@
                                 <li><a href="{{ url('/logout') }}" onclick="
 								event.preventDefault();
 								document.getElementById('logout-form').submit();">
-                                        Logout</a></li>
+                                        Logout</a>
+                                    <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">{{ csrf_field() }} </form>
+                                </li>
 
                             </ul>
                         </li>
