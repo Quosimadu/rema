039b23f75350aac179ad8f6f960b944ce8dad69f
Quosimadu <paul.wehle@pcwehle.de>
2017-10-21 23:53:16 +0200

    laravel 5.5 upgrade refs #11

--- config/app.php
+++ config/app.php
@@ -179,7 +179,7 @@ return [
         Rap2hpoutre\LaravelLogViewer\LaravelLogViewerServiceProvider::class,
         SimpleSoftwareIO\SMS\SMSServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
-        Sofa\Eloquence\ServiceProvider::class,
+        Sofa\Eloquence\BaseServiceProvider::class,
 
 
     ],
