1b0ece31be01d46ec0dfc9752a32919c296c9972
Paul Wehle <paul.wehle@pcwehle.de>
2018-04-26 14:00:38 +0200

    mail formatting for forwarding refs #2

--- resources/views/emails/message_to_email.blade.php
+++ resources/views/emails/message_to_email.blade.php
@@ -1,10 +1,9 @@
 <h2>SMS message received</h2>
-
 @component('mail::panel')
-<p>At: {{ $textMessage->created_at }}<br/>
-From: {{ $textMessage->sender }}<br/>
-To: {{ $textMessage->receiver }}<br/>
-Message: {{ $textMessage->content }}</p>
+    Message: {{ $textMessage->content }}
 @endcomponent
+<p>At: {{ $textMessage->created_at }}<br/>
+    From: {{ $textMessage->sender }}<br/>
+    To: {{ $textMessage->receiver }}</p>
 
 <p><i>Sent by {{ config('app.name') }}</i></p>
